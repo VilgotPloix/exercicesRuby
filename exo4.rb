f4f3c8935823201a8ce1145d08b96c180702c8ff
Gustave PLOIX <vilgotploix@gmail.com>
2021-01-12 22:39:56 +0100

    correction de l'exo4

--- exo4.rb
+++ exo4.rb
@@ -1,10 +1,7 @@
-#Le programme exo_11.rb est cool, mais on peut l'améliorer. Écris un programme exo_16.rb qui va demander son âge à l'utilisateur, et qui, pour chaque année depuis sa naissance, dira "Il y a X ans, tu avais Y ans".
-
-
-puts "quelle est votre date de naissance ? "
+puts "quelle est votre année de naissance ? "
 
 annee = gets.chomp.to_i
 
-i = {2021 - annee}
+i = annee + 100
 
-puts i 
+puts "vous aurez 100 ans en #{i}"
